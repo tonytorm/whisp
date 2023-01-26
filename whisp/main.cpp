f288bdad88adaeb00207dcd92b7b6c4450cce383
tonytorm <giulio2.iacomino@live.uwe.ac.uk>
2023-01-26 11:12:03 +0000

    Initial Commit

---
+++ whisp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  whisp
+//
+//  Created by Giulio Iacomino on 26/01/2023.
+//  Copyright © 2023 Giulio Iacomino. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
